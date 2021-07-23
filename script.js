904e6916bfbbcbe4e16256233d0f64b27cc14281
DigvijaySingh18 <digvijaysingh1898@gmail.com>
2021-07-23 08:02:29 +0530

    Added Refresh Button

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const container = document.querySelector("#container");
-
-const body = document.querySelector("body");
-// let noOfDivs = 256;
-
-let rowDivs = 16;
-let columnDivs = 16;
-
 function createDiv(rowDiv, columnDiv){
     for (let i=0; i<rowDiv*columnDiv; i++){
         let newDiv = document.createElement("div");
@@ -16,24 +8,50 @@ function createDiv(rowDiv, columnDiv){
     container.style.gridTemplateRows = `repeat(${rowDiv}, 1fr)`;
 }
 
+function addColor(e){
+    e.target.setAttribute("class", "colorDiv");
+}
+
+function replaceDivs(){
+    rowDivs = parseInt(prompt("How many row Divs do u want?"));
+    columnDivs = parseInt(prompt("How many Column Divs do u want?"));
+
+    if (rowDivs > 100 || columnDivs > 100){
+        alert('Value Greater than 100\nMaximum allowed value is 100');
+        return;
+    }
+
+    let childDivs = container.querySelectorAll('div');
+    childDivs.forEach(childDiv => container.removeChild(childDiv));
+    createDiv(rowDivs, columnDivs);
+
+    let addDivs = document.querySelectorAll(".div");
+    addDivs.forEach(addDiv => addDiv.addEventListener('mouseenter', addColor));
+}
+
+
+function removeColor(){
+    sketchDivs.forEach( sketchDiv => sketchDiv.removeAttribute("class"))
+}
+
+
+
+const container = document.querySelector("#container");
+const body = document.querySelector("body");
+
+let rowDivs = 16;
+let columnDivs = 16;
+
+
 createDiv(rowDivs, columnDivs);
 
 
-
-
-
 const sketchDivs = document.querySelectorAll(".div");
-
 sketchDivs.forEach( sketchDiv => sketchDiv.addEventListener('mouseenter', addColor))
-
-function addColor(e){
-    console.log(e.target);
-    e.target.setAttribute("class", "colorDiv")
-}
 
 const resetPad = document.createElement('button');
 resetPad.innerHTML = "Reset";
-resetPad.setAttribute("style", "height: 30px; width: 60px;")
+resetPad.setAttribute("style", "height: 40px; width: 80px; margin-right: 60px;");
 body.insertBefore(resetPad, container);
 
 resetPad.addEventListener("click", () => {removeColor();
@@ -41,23 +59,9 @@ resetPad.addEventListener("click", () => {removeColor();
 });
 
 
-function removeColor(){
-    sketchDivs.forEach( sketchDiv => sketchDiv.removeAttribute("class"))
-}
+const refreshPad = document.createElement('button');
+refreshPad.innerHTML = "Refresh";
+refreshPad.setAttribute("style", "height: 40px; width: 80px; margin-left: 60px");
+body.insertBefore(refreshPad, container);
 
-function replaceDivs(){
-    rowDivs = parseInt(prompt("How many row Divs do u want?"));
-    columnDivs = parseInt(prompt("How many Column Divs do u want?"));
-    console.log(rowDivs, columnDivs);
-
-    if (rowDivs > 100 || columnDivs > 100){
-        alert('Value Greater than 100\nMaximum allowed value is 100');
-        return;
-    }
-    let childDivs = container.querySelectorAll('div');
-    childDivs.forEach(childDiv => container.removeChild(childDiv));
-    // container.removeChild('div');
-    createDiv(rowDivs, columnDivs);
-    let addDivs = document.querySelectorAll(".div");
-    addDivs.forEach(addDiv => addDiv.addEventListener('mouseenter', addColor));
-}
+refreshPad.addEventListener("click", removeColor);
